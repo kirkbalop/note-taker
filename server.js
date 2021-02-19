5f25af7867ad8a728722d82a0b44677c5fc698e3
Kirk <kirkbalop@gmail.com>
2021-02-18 17:25:32 -0800

    added routing js files and dependencies

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 // Dependencies
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 var app = express();
 var PORT = process.env.PORT || 3001;
